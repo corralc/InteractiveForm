f4b4c5349a77b336eb3689e51337129dbcd988f9
Cristian Corrales <cristiancorrales84@gmail.com>
2020-08-23 22:24:48 -0400

    Added T shirt info functionality

--- js/script.js
+++ js/script.js
@@ -1,24 +1,57 @@
 const jobRoleText = document.querySelector("#other-title");
+const shirtColor = document.querySelector("#color");
+const designDrop = document.querySelector("#design");
 
 
 const firstLoad = () =>{ 
     const nameText = document.querySelector("#name");
-    
-    
+
     nameText.focus();
     jobRoleText.style.display = "none";
-
+    checkDesign("Select Theme");
 }
+
+const checkDesign = (design = "Select Theme") =>{
+    const newSelectOption = document.createElement("option");
+    let ArrSelectOptions = [];
+
+    newSelectOption.textContent = "Please Select a T-Shirt theme";
+    newSelectOption.value = "select shirt";
+    
+    let selectOptions = document.querySelectorAll("#color option");
+    
+    
+    if(design === "Select Theme"){
+        for(let i =0; i < selectOptions.length; i++){
+            selectOptions[i].style.display = "none";
+        }
+        shirtColor.appendChild(newSelectOption);
+        shirtColor.value = "select shirt";
+    }
+  else{
+        for(let i =0; i < selectOptions.length; i++){
+            selectOptions[i].style.display = "none";
+
+            if(design === "js puns" && selectOptions[i].textContent.includes("JS Puns")){  
+                selectOptions[i].style.display = "block";
+                ArrSelectOptions.push(selectOptions[i]);
+            }
+            else if(design === "heart js" && selectOptions[i].textContent.includes("JS shirt only")){
+                selectOptions[i].style.display = "block";
+                ArrSelectOptions.push(selectOptions[i]);
+            }
+    }
+    selectOptions[0].parentNode.value = ArrSelectOptions[0].value;
+}
+    
+
+};
+
+
 
 firstLoad();
 
 const selectInput = document.querySelector("#title");
-
-if(selectInput.value === "other"){
-    
-    console.log("Other has been selected");
-
-}
 
 
 selectInput.addEventListener("change", (e) =>{
@@ -30,6 +63,10 @@ selectInput.addEventListener("change", (e) =>{
     else{
         jobRoleText.style.display = "none";
     }
+});
+
+designDrop.addEventListener("change", (e)=>{
+     checkDesign(e.target.value);
 });
 
 
